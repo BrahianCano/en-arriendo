fb713bef7f1a5cc5ef12c6b5b6c7ceb7d3fa253b
brahian <brahian.stiben@talent.com>
2020-12-07 16:53:29 -0500

    Structure Atom Desing

---
+++ components/organisms/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+// Import resorces //
+import NavbarAnchors from '../molecules/navbar-anchors';
+import NavbarLogo from '../molecules/navbar-logo'
+import NavbarBtnlogin from '../molecules/navbar-btnlogin'
+import NavbarMobile from '../molecules/navbar-mobile'
+
+
+export default function Navbar() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  return (
+    <div className="relative bg-white">
+      
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+
+          {/** <!--Mobile menu, show/hide based on mobile menu state.--> */}
+          <NavbarLogo />
+
+          {/** <!--Mobile menu, show/hide based on mobile menu state.--> */}
+          <div className="-mr-2 -my-2 md:hidden">
+            <button type="button" onClick={() => setIsOpenMenu(true)} className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+
+          {/** <!--Mobile menu, show/hide based on mobile menu state.--> */}
+          <NavbarAnchors />
+
+          {/** <!--Mobile menu, show/hide based on mobile menu state.--> */}
+          <NavbarBtnlogin />
+
+        </div>
+      </div>
+
+      {/** <!--Mobile menu, show/hide based on mobile menu state.--> */}
+      <NavbarMobile isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
+
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+
+import Navbar from "../components/organisms/Navbar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        {/** <!-- SEO / Home --> */}
+        <title>PENDIENTE ESTUDIO SEO</title>
+        <meta name="description" content="PENDIENTE ESTUDIO SEO" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="index, follow" />
+
+        {/** <!-- Open Graph / Facebook --> */}
+        <meta property="og:title" content="PENDIENTE SEO" key="title" />
+        <meta
+          property="og:description"
+          content="PENDIENTE SEO"
+          key="description"
+        />
+        <meta
+          property="og:image"
+          content="https://midominio.com/logo200x200.png"
+        />
+        <meta property="og:url" content="https://midominio.com/" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Navbar />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    </>
+  );
 }
