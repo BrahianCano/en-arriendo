9825aa299240e09efc78875b733b86a08f2da022
BrahianCano <brahianestiven08@gmail.com>
2021-01-02 18:08:03 -0500

    Update structure in serch component

--- components/molecules/Search.jsx
+++ components/molecules/Search.jsx
@@ -1,26 +1,42 @@
-import IconPublic from "../../assets/svg/icon-apartament";
+// Import icons svg //
+import IconApartament from "../../assets/svg/icon-apartament";
+
 
 export default function Search() {
     return (
-            <form>
-                <div className="mt-1 relative rounded-md shadow-sm">
-                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <span className="ml-0 sm:text-sm">
-                       <IconPublic/>
-                      </span>
+            <form className="md:flex md:justify-center grid grid-cols-1">
+
+                    <div className="relative rounded-md shadow-sm">
+
+                             {/** <!--Icon input.--> */}
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                               <IconApartament />
+                            </div>
+
+                             {/** <!--Input search.--> */}
+                            <input type="search" name="location"
+                                   className="focus:ring-primary focus:border-primary block w-full pl-12 pr-36 sm:text-sm border-gray-300 rounded-md"
+                                   placeholder="Ciudad, Barrio"/>
+
+                             {/** <!--Select category.--> */}
+                            <div className="absolute inset-y-0 right-0 flex items-center">
+                                    <label htmlFor="category" className="sr-only">Category</label>
+                                    <select name="category"
+                                            className="focus:ring-primary focus:primary h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
+                                        <option>Apartamento</option>
+                                        <option>Casa</option>
+                                        <option>Local</option>
+                                        <option>Finca</option>
+                                    </select>
+                            </div>
                     </div>
-                    <input type="text" name="price" id="price"
-                           className="ml-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-                           placeholder="¿Que buscas?"/>
-                    <div className="absolute inset-y-0 right-0 flex items-center">
-                        <label htmlFor="currency" className="sr-only">Currency</label>
-                        <select id="Currency" name="currency"
-                                className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                            <option>Medellin</option>
-                            <option>Itagui</option>
-                            <option>Sabaneta</option>
-                        </select>
-                    </div>
+
+                {/** <!--Button search.--> */}
+                <div className="my-3 md:my-0 mx-0 md:mx-5">
+                        <input
+                            className="whitespace-nowrap w-full md:w-32 inline-flex items-center justify-center px-4 py-1.5 border border-transparent rounded-md shadow-sm text-base text-white bg-primary hover:bg-primaryDark"
+                            value="Buscar"
+                            type="submit"/>
                 </div>
             </form>
     );
--- components/organisms/HeaderLanding.jsx
+++ components/organisms/HeaderLanding.jsx
@@ -1,24 +1,26 @@
 // Import molecules //
 import Search from "../molecules/Search";
 
-// Import icons svg //
-import IconPublic from "../../assets/svg/icon-apartament";
-
 
 export default function HeaderLanding() {
     return (
-        <section className="my-20 mx-10 font-axiformaMedium">
-            <h1 className="text-4xl text-center">
-                Titulo<span className="text-primary font-axiformaHeavy"> SEO</span>
-                <br/>
-                para posicionamiento
-            </h1>
+        <section className="my-20 font-axiformaMedium">
 
-            <h4 className="text-sm text-center my-3 text-gray-500	">
-                Slogan y llamado a la accion
-            </h4>
+            <div className="mx-10 text-center">
+                <h1 className="text-4xl md:text-5xl">
+                    Titulo<span className="text-primary font-axiformaHeavy"> SEO</span>
+                    <br/>
+                    para posicionamiento
+                </h1>
 
-            <div className="my-20">
+                <h4 className="my-3 text-gray-500">
+                    Slogan y llamado a la accion
+                </h4>
+            </div>
+
+            <div className="my-20 mx-5 md:mx-40">
+
+                {/** <!-- Search action.--> */}
                 <Search/>
             </div>
 
