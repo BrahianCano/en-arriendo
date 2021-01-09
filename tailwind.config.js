2a273c69ddbb35d121d2cf99abd07f2a0c6a804b
BrahianCano <brahianestiven08@gmail.com>
2021-01-09 12:53:13 -0500

    Change configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: [],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   plugins: [
