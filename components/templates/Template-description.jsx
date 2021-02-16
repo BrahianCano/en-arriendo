52abf95984b12a585b36ef6a0abfbd672f27e7f9
BrahianCano <brahian.stiben@talent.com>
2021-02-16 18:27:38 -0500

    Create section Description

--- components/templates/Template-description.jsx
+++ components/templates/Template-description.jsx
@@ -4,7 +4,7 @@ import Mosaic from "../molecules/Mosaic";
 export default function TemplateDescription() {
     return (
         <>
-            <div className="font-axiformaMedium w-full flex flex-col justify-center">
+            <div className="mt-20 font-axiformaMedium w-full flex flex-col justify-center">
                 <section>
                     <Mosaic/>
                 </section>
